52717da2c0f253587e47ebf640635c13404677c8
Kryštof Háva <Krystof.Hava@hotmail.com>
2024-06-17 10:12:07 +0200

    ADDED encoding_1 project with new .gitignore file

---
+++ encoding/encoding_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace encoding_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream("text.txt", FileMode.Open))
+            {
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, bytes.Length);
+                List<uint> values = UTF8_Coder.Decode(bytes);
+                foreach (int value in values)
+                {
+                    Console.WriteLine(value);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ encoding/encoding_1/UTF_Coder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace encoding_1
+{
+    public class UTF8_Coder
+    {
+        public static List<uint> Decode(byte[] bytes)
+        {
+            List<uint> values = new List<uint>();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                byte b = bytes[i];
+                if (b < 128)
+                {
+                    values.Add(b);
+                }
+                else
+                {
+                    byte mask = 63;
+                    int count = 1;
+                    while ((b &= mask) > (mask >>= 1)) count++;
+                    uint value = b;
+                    for (int j = 1; j <= count; j++)
+                    {
+                        value <<= 6;
+                        value |= (uint)bytes[j + i] & 63;
+                    }
+                    i += count;
+                    values.Add(value);
+                }
+            }
+            return values;
+        }
+        public static List<byte> Encode(List<uint> values)
+        {
+            List<byte> bytes = new List<byte>();
+            foreach (uint value in values)
+            {
+                if (value <= 255)
+                {
+                    bytes.Add((byte)value);
+                }
+            }
+            return bytes;
+        }
+    }
+}
